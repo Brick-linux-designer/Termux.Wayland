ddfd5f6500730279e9a3aaf016b68f80cf2607e5
Brick-linux-designer <edouardlequoq@gmail.com>
2026-07-07 10:59:30 +0200

    Add files via upload

---
+++ Java_version/app/src/main/java/com/termux/wayland/data/LinuxApp.java
@@ -0,0 +1,25 @@
+package com.termux.wayland.data;
+
+public class LinuxApp {
+
+    public String id;
+
+    public String name;
+
+    public String packageName;
+
+    public String exec;
+
+    public String category;
+
+    public String icon;
+
+    public boolean requiresDisplay = true;
+
+    public String description;
+
+    public String repo;
+
+    public LinuxApp() {
+    }
+}
